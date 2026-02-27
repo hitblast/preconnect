ea17255f22ecdbaf7f1baec4ad58fb7464d21eb1
sabbirba <sabbirba10@gmail.com>
2026-02-28 01:09:15 +0600

    Remove deprecated seatstatus worker directory

--- web/seatstatus/src/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Env {
-  WORKER_REFRESH_TOKEN?: string;
-  WORKER_ACCESS_TOKEN?: string;
-}
-
-export type SeatMap = Record<string, unknown>;
-export type JsonObject = Record<string, unknown>;
